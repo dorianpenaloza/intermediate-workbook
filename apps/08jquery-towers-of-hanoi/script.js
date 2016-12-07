500cb178dd4b3d3f25e6f4f9043078e5d40abe1d
dorianpenaloza <dorianpenaloza@yahoo.com>
2016-12-06 19:42:16 -0600

    updated announce message

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,16 +1,15 @@
 'use strict';
-
-$(document).ready(function() {
+$(document).on('ready', function() {
   var $block = null;
   $('[data-stack]').click (function () {
     var $children = $(this).children();
     if ($block === null) {
-      if ($children.length > 0) {
+      if ($children.length > 1) {
         $block = $children.last().detach();
       }
     }
     else {
-      if ($children.length === 0 || $children.last().data('block') > $block.data('block')) {
+      if ($children.length === 1 || $children.last().data('block') > $block.data('block')) {
         $(this).append($block);
         $block = null;
       }
@@ -20,8 +19,10 @@ $(document).ready(function() {
 
   function checkForWin() {
      $('[data-stack]').each(function() {
-      if (($(this).data('stack') > 1) && ($(this).children().length === 4)) {
-            $('#announce-game-won').text('You Won Pomsky!');
+      if (($(this).data('stack') > 2) && ($(this).children().length === 5)) {
+            $('#announce-congrats').text('Congrats');
+            $('#announce-you').text('You');
+            $('#announce-won').text('Won!');
       }
     });
   }
